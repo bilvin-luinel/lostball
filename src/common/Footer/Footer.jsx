1aa1fbb6d533af458f30dc23e92d2c2e6c9b95eb
king gi bum <usn9514@naver.com>
2025-02-28 18:30:01 +0900

    t

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -11,13 +11,12 @@ const Footer = () => {
                 <img className={ss.logo} src={`${process.env.PUBLIC_URL}/logo.png`} alt='' />
                 <div className={ss.body}>
                     <p className={ss.info_line}>
-                        <span><span className={ss.lato}>Y-Jun</span> export trading company&nbsp;|</span>
-                        <span>Registration No : 220-04-99901<span className={ss.only_pc}>&nbsp;|</span></span>
-                        <span>Tel : +82-31-945-5636&nbsp;|</span>
-                        <span>FAX : +82-31-945-5637</span>
+                        <span><span className={ss.lato}>전 브랜드</span> 골프용품&nbsp;|</span>
+                        <span>사업자번호 : 495-22-02023<span className={ss.only_pc}>&nbsp;|</span></span>
+                        <span>Tel : 031-922-0604&nbsp;|</span>
                     </p>
                     <p className={ss.info_line}>
-                        <span>#3, 55, Solan-gil, Sangjiseok-dong, Paju-si, <span className={ss.only_m}><br /></span>Gyeonggi-do, S/Korea<span className={ss.only_pc}>&nbsp;|</span></span>
+                        <span>경기도 일산서구 탄중로111, <span className={ss.only_m}><br /></span>Gyeonggi-do, S/Korea<span className={ss.only_pc}>&nbsp;|</span></span>
                         <span>ZIP code(postcode) : 10911</span>
                     </p>
                     <p className={ss.info_line}>
